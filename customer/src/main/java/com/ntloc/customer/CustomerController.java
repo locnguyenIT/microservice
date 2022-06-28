db05a8fea5d295210ca61ad918ec923b5998fe51
ntloc <ntloc.developer@gmail.com>
2022-06-28 22:36:40 +0700

    customer-microservice

---
+++ customer/src/main/java/com/ntloc/customer/CustomerController.java
@@ -0,0 +1,26 @@
+package com.ntloc.customer;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static com.ntloc.customer.CustomerConstant.URI_REST_API_VERSION_CUSTOMER;
+
+@AllArgsConstructor
+@Slf4j
+@RestController
+@RequestMapping(path = URI_REST_API_VERSION_CUSTOMER)
+public class CustomerController {
+
+    private final CustomerService customerService;
+
+    @PostMapping(path = "/registration")
+    public CustomerDTO register(@RequestBody CustomerDTO customerDTO) {
+        log.info("New customer registration {}", customerDTO);
+        return customerService.register(customerDTO);
+    }
+
+}
---
+++ customer/src/main/java/com/ntloc/customer/CustomerService.java
@@ -0,0 +1,26 @@
+package com.ntloc.customer;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Slf4j
+@AllArgsConstructor
+@Service
+public class CustomerService {
+
+    private final CustomerRepository customerRepository;
+    private final CustomerMapper customerMapper;
+
+    public CustomerDTO register(CustomerDTO customerDTO) {
+        CustomerEntity customer = CustomerEntity.builder()
+                .name(customerDTO.getName())
+                .email(customerDTO.getEmail())
+                .build();
+        CustomerEntity save = customerRepository.save(customer);
+        log.info("Customer register success {}", save);
+        return customerMapper.toDTO(save);
+    }
+
+}
